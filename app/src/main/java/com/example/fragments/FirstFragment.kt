450a8c62e27446387c6e3cceee59d08698cfde6e
Nabeel Ahmad <nabeel.oraxtech@gmail.com>
2024-01-31 13:26:55 +0500

    Added DataPassing by bundle

--- app/src/main/java/com/example/fragments/FirstFragment.kt
+++ app/src/main/java/com/example/fragments/FirstFragment.kt
@@ -1,9 +1,33 @@
 package com.example.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 
 class FirstFragment:Fragment(R.layout.fragment_first){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view=inflater.inflate(R.layout.fragment_first,container,false)
+        val btn:Button=view.findViewById(R.id.btnFragment1)
+        btn.setOnClickListener {
+            val editText=view.findViewById<EditText>(R.id.etFragment1)
+            val input=editText.text.toString()
+            val bundle=Bundle()
+            bundle.putString("data",input)
 
+            val fragment=SecondFragment()
+            fragment.arguments=bundle
+            fragmentManager?.beginTransaction()?.add(R.id.fragment,fragment)?.commit()
+        }
+        return view
+    }
 }
 
 
--- app/src/main/java/com/example/fragments/SecondFragment.kt
+++ app/src/main/java/com/example/fragments/SecondFragment.kt
@@ -1,6 +1,23 @@
 package com.example.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class SecondFragment:Fragment(R.layout.fragment_second) {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view=inflater.inflate(R.layout.fragment_second,container,false)
+        val textView:TextView=view.findViewById(R.id.tvFragment2)
+        val args=this.arguments
+        val inputData=args?.get("data")
+        textView.text=inputData.toString()
+        return view
+    }
 }
